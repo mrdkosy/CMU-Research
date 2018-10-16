4fac2f69d283663c6711ff909c057924cc6369f5
STUDIO <STUDIO@STUDIOs-MacBook-Pro.local>
2018-10-16 12:44:45 -0400

    refactoring backup

--- mainProject/src/ComputerVision.hpp
+++ mainProject/src/ComputerVision.hpp
@@ -32,7 +32,7 @@ private:
     ofFbo cvIronFilingsImage, goalImage;
     ofImage peopleTestImage, ironfilingsTestImage;
     ofxCvGrayscaleImage grayIronFilingsImage, grayPeopleImage;
-    ofxCvColorImage colorIronFilingsImage, colorPeopleImage;
+    ofxCvColorImage colorIronFilingsImage, colorPeopleImage, trimmedIronFilingsImage;
     ofRectangle trimmedArea; //trimming the image from iron filings camera
     bool isTrimmingMode;
     vector<ofVec2f> trimmedPosition;
@@ -46,6 +46,7 @@ private:
         grayPeopleImage.allocate(WIDTH_PROCESS, HEIGHT_PROCESS);
         colorIronFilingsImage.allocate(WIDTH_PROCESS, HEIGHT_PROCESS);
         colorPeopleImage.allocate(WIDTH_PROCESS, HEIGHT_PROCESS);
+        trimmedIronFilingsImage.allocate(WIDTH_PROCESS, HEIGHT_PROCESS);
         
         
 #ifdef REALTIME_CAPTURE_PEOPLE
@@ -137,7 +138,6 @@ private:
         ofPushMatrix();
         ofTranslate(0, HEIGHT_VIEW);
         trimImage();
-        
         drawText("real image of iron filings from camera");
         
         ofPopMatrix();
@@ -147,14 +147,15 @@ private:
          cv iron filings
          *******************/
         cvIronFilingsImage.begin();
-        grayIronFilingsImage = colorIronFilingsImage;
+        if(trimmedArea.isEmpty() || isTrimmingMode) grayIronFilingsImage = colorIronFilingsImage;
+        else grayIronFilingsImage = trimmedIronFilingsImage;
         grayIronFilingsImage.draw(0,0);
         cvIronFilingsImage.end();
         
         ofPushMatrix();
         ofTranslate(WIDTH_VIEW, HEIGHT_VIEW);
         cvIronFilingsImage.draw(0, 0, WIDTH_VIEW, HEIGHT_VIEW);
-        drawGrid();
+        //drawGrid();
         drawText("cv image of iron filings");
         ofPopMatrix();
         
@@ -162,33 +163,39 @@ private:
     //--------------------------------------------------------------
     void trimImage(){
         ofPushStyle();
-
+        
         
         if(isTrimmingMode){
-            cvIronFilingsImage.draw(0, 0, WIDTH_VIEW, HEIGHT_VIEW);
-            if(trimmedArea.isEmpty()){
-                colorIronFilingsImage.draw(0,0,WIDTH_VIEW, HEIGHT_VIEW);
-                ofFill();
-                ofSetColor(0, 100);
-                ofDrawRectangle(0,0,WIDTH_VIEW, HEIGHT_VIEW);
-                ofSetColor(255, 255);
-                ofDrawBitmapString("select trimming area with mouse click", 180, HEIGHT_VIEW/2);
-                
-            }else{
-                colorIronFilingsImage.draw(0,0,WIDTH_VIEW, HEIGHT_VIEW);
-                
+            ofSetColor(255, 255);
+            colorIronFilingsImage.draw(0,0,WIDTH_VIEW, HEIGHT_VIEW);
+            ofFill();
+            ofSetColor(0, 100);
+            ofDrawRectangle(0,0,WIDTH_VIEW, HEIGHT_VIEW);
+            ofSetColor(255, 255);
+            ofDrawBitmapString("select trimming area with mouse click", 180, HEIGHT_VIEW/2);
+            
+            ofSetColor(0,0,200);
+            for(int i=0; i<trimmedPosition.size(); i++){
+                ofDrawCircle(trimmedPosition[i].x, trimmedPosition[i].y, 5);
+            }
+            if(!trimmedArea.isEmpty()){
                 ofNoFill();
                 ofSetColor(0,0,200);
                 ofDrawRectangle(trimmedArea);
+                
             }
             
         }
         else{
-            //            if(trimmedArea.isEmpty()){ // don't set the area yet
-            //                cvIronFilingsImage.draw(0, 0, WIDTH_VIEW, HEIGHT_VIEW);
-            //            }else{
-            //                colorIronFilingsImage.setROI(trimmedArea);
-            //            }
+            ofSetColor(255, 255);
+            if(trimmedArea.isEmpty()){ // don't set the trimming area yet
+                cvIronFilingsImage.draw(0, 0, WIDTH_VIEW, HEIGHT_VIEW);
+            }else{
+                trimmedIronFilingsImage = colorIronFilingsImage;
+                trimmedIronFilingsImage.setROI(trimmedArea);
+                trimmedIronFilingsImage.resize(WIDTH_PROCESS, HEIGHT_PROCESS);
+                trimmedIronFilingsImage.drawROI(0, 0, WIDTH_VIEW, HEIGHT_VIEW);
+            }
             
         }
         ofPopStyle();
@@ -238,15 +245,27 @@ public:
     }
     //--------------------------------------------------------------
     void mousePressed(int x, int y){
+        
         if(isTrimmingMode){
-            if(trimmedPosition.size() < 2) trimmedPosition.push_back(ofVec2f(x,y));
-            if(trimmedPosition.size() == 2){
-                float x = trimmedPosition[0].x;
-                float y = trimmedPosition[0].y;
-//                trimmedArea.set()
+            if(trimmedPosition.size() < 2){
+                int _x = x;
+                int _y = y-HEIGHT_VIEW;
+                
+                if(trimmedPosition.size() == 1){
+                    _y = ((float)HEIGHT_PROCESS/WIDTH_PROCESS)*(_x-trimmedPosition[0].x) + trimmedPosition[0].y;
+                }
+                trimmedPosition.push_back(ofVec2f(_x, _y));
             }
-            
+            if(trimmedPosition.size() == 2){
+                float _x = trimmedPosition[0].x;
+                float _y = trimmedPosition[0].y;
+                float _w = abs(trimmedPosition[1].x - trimmedPosition[0].x);
+                float _h = abs(trimmedPosition[1].y - trimmedPosition[0].y);
+                trimmedArea.setPosition(_x, _y);
+                trimmedArea.setSize(_w, _h);
+            }
         }
+        
     }
     
 };
